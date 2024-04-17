f9c97fbba60e5204a851c1095821afeada2a9216
mai <74494945+chan-mai@users.noreply.github.com>
2024-04-17 15:26:10 +0000

    add: 応募ページ

---
+++ src/pages/Audition.vue
@@ -0,0 +1,166 @@
+<template>
+    <!-- Hero -->
+    <div class="relative overflow-hidden bg-white">
+        <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10">
+            <div class="max-w-2xl text-center mx-auto">
+            <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl dark:text-white">一緒の未来を信じて輝ける。<br />そんな<span class="text-rose-500">仲間</span>を探しています。</h1>
+            <br />
+            <p class="mt-3 text-lg text-gray-600">まだ始まったばかりの小さな事務所ですが、一緒に未来を信じて進んでいける。<br />そんな仲間を、私たちは探しています。</p>
+            </div>
+
+            <div class="mt-10 relative max-w-5xl mx-auto">
+            <div class="w-full object-cover h-96 sm:h-[480px] bg-[url('https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-1-580x440.png')] bg-no-repeat bg-center bg-cover rounded-xl"></div>
+
+            <div class="absolute inset-0 size-full">
+                <div class="flex flex-col justify-center items-center size-full">
+                <a class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800" href="#">
+                    <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"/></svg>
+                    詳しく見る
+                </a>
+                </div>
+            </div>
+
+            <div class="absolute bottom-12 -start-20 -z-[1] size-48 bg-gradient-to-b from-orange-500 to-white p-px rounded-lg dark:to-neutral-900">
+                <div class="bg-white size-48 rounded-lg dark:bg-neutral-900"></div>
+            </div>
+
+            <div class="absolute -top-12 -end-20 -z-[1] size-48 bg-gradient-to-t from-blue-600 to-cyan-400 p-px rounded-full">
+                <div class="bg-white size-48 rounded-full dark:bg-neutral-900"></div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Hero -->
+  
+    <div class="container mx-auto px-10">
+        <h1 class="mt-10 text-7xl font-bold text-gray-800">REQUIREMENTS</h1>
+        <h2 class="mb-5 text-2xl font-bold text-rose-400">応募条件</h2>
+        <hr>
+
+        <!-- 01 必須条件 -->
+        <h3 class="mt-10 mb-5 text-4xl font-bold text-gray-800 dark:text-gray-200"><span class="mt-10 text-2xl text-rose-400">01</span> 必須条件</h3>
+        <div class="grid grid-cols-3 gap-4">
+            <div class="bg-white rounded-lg p-4">
+                <div class="flex">
+                    <div class="flex items-center justify-center rounded-full w-11 h-11 bg-rose-200">
+                        <span class="font-semibold">1</span>
+                    </div>
+                    <p class="ml-2 mt-2 text-gray-800 dark:text-gray-200"><b>条件要約</b></p>
+                </div>
+                <p class="mt-2 text-gray-800 dark:text-gray-200">条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 </p>
+            </div>
+
+            <div class="bg-white rounded-lg p-4">
+                <div class="flex">
+                    <div class="flex items-center justify-center rounded-full w-11 h-11 bg-rose-200">
+                        <span class="font-semibold">2</span>
+                    </div>
+                    <p class="ml-2 mt-2 text-gray-800 dark:text-gray-200"><b>条件要約</b></p>
+                </div>
+                <p class="mt-2 text-gray-800 dark:text-gray-200">条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 </p>
+            </div>
+            
+            <div class="bg-white rounded-lg p-4">
+                <div class="flex">
+                    <div class="flex items-center justify-center rounded-full w-11 h-11 bg-rose-200">
+                        <span class="font-semibold">3</span>
+                    </div>
+                    <p class="ml-2 mt-2 text-gray-800 dark:text-gray-200"><b>条件要約</b></p>
+                </div>
+                <p class="mt-2 text-gray-800 dark:text-gray-200">条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 条件詳細 </p>
+            </div>
+        </div>
+
+
+        
+        <!-- 02 歓迎条件 -->
+        <h3 class="mt-10 mb-5 text-4xl font-bold text-gray-800 dark:text-gray-200"><span class="mt-10 text-2xl text-rose-400">02</span> 歓迎条件</h3>
+        <ul class="list-disc list-inside ml-5">
+            <li class="mt-2 text-gray-800 dark:text-gray-200">
+                条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 
+            </li>
+            <li class="mt-2 text-gray-800 dark:text-gray-200">
+                条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 
+            </li>
+            <li class="mt-2 text-gray-800 dark:text-gray-200">
+                条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 
+            </li>
+            <li class="mt-2 text-gray-800 dark:text-gray-200">
+                条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 条件 
+            </li>
+        </ul>
+
+        
+        <!-- 03 審査の流れ -->
+        <h3 class="mt-10 mb-5 text-4xl font-bold text-gray-800 dark:text-gray-200"><span class="mt-10 text-2xl text-rose-400">03</span> 審査の流れ</h3>
+        <div class="flex">
+            <div class="grow bg-white rounded-lg p-4">
+                <div class="flex">
+                    <div class="flex items-center justify-center rounded-full w-11 h-11 bg-rose-200">
+                        <span class="font-semibold">1</span>
+                    </div>
+                    <p class="ml-2 mt-2 text-gray-800 dark:text-gray-200"><b>見出し</b></p>
+                </div>
+                <p class="mt-2 text-gray-800 dark:text-gray-200">概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 </p>
+            </div>
+            <div class="grow-0 mr-5 ml-5 flex items-center justify-center">
+                ▷
+            </div>
+            <div class="grow bg-white rounded-lg p-4">
+                <div class="flex">
+                    <div class="flex items-center justify-center rounded-full w-11 h-11 bg-rose-200">
+                        <span class="font-semibold">2</span>
+                    </div>
+                    <p class="ml-2 mt-2 text-gray-800 dark:text-gray-200"><b>見出し</b></p>
+                </div>
+                <p class="mt-2 text-gray-800 dark:text-gray-200">概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 </p>
+            </div>
+            <div class="grow-0 mr-5 ml-5 flex items-center justify-center">
+                ▷
+            </div>
+            <div class="grow bg-white rounded-lg p-4">
+                <div class="flex">
+                    <div class="flex items-center justify-center rounded-full w-11 h-11 bg-rose-200">
+                        <span class="font-semibold">3</span>
+                    </div>
+                    <p class="ml-2 mt-2 text-gray-800 dark:text-gray-200"><b>見出し</b></p>
+                </div>
+                <p class="mt-2 text-gray-800 dark:text-gray-200">概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 概要説明 </p>
+            </div>
+            
+        </div>
+
+        
+        <!-- 04 応募方法 -->
+        <h3 class="mt-10 mb-5 text-4xl font-bold text-gray-800 dark:text-gray-200"><span class="mt-10 text-2xl text-rose-400">04</span> 応募方法</h3>
+        <div class="bg-white rounded-lg p-4">
+            <p>ENTRYボタンからフォームにアクセスし、必要事項をご記入のうえご応募ください。</p>
+            <!--グラデーションボタン 丸角 ホバー時にアニメーション-->
+            
+            <a href="#" class="mt-5 inline-block py-3 px-36 text-sm font-semibold text-white bg-gradient-to-r from-pink-400 to-rose-400 rounded-full shadow-sm hover:from-pink-600 hover:to-rose-600">ENTRY&nbsp;&nbsp;▷</a>
+        </div>
+
+        <div class="mt-10 bg-white rounded-lg p-4">
+            <div class="flex">
+                <div class="flex items-center justify-center rounded-full w-11 h-11 bg-rose-200">
+                    <span class="font-semibold">！</span>
+                </div>
+                <p class="ml-2 mt-2 mb-2 text-gray-800 dark:text-gray-200"><b class="text-2xl">注意事項</b></p>
+            </div>
+            <ul class="list-disc list-inside ml-5">
+                <li class="mt-2 text-gray-800 dark:text-gray-200">
+                    虚偽の申告が発覚した場合、合格を取り消しとさせていただく場合がございます。
+                </li>
+                <li class="mt-2 text-gray-800 dark:text-gray-200">
+                    選考結果は通過者のみにご連絡いたします。
+                </li>
+                <li class="mt-2 text-gray-800 dark:text-gray-200">
+                    お送りいただいた個人情報は選考以外の目的で使用することはございません。
+                </li>
+                <li class="mt-2 text-gray-800 dark:text-gray-200">
+                    審査状況や選考結果に関するお問い合わせは受け付けておりません。
+                </li>
+            </ul>
+        </div>
+    </div>
+  </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from "../pages/Index.vue";
+import Audition from "../pages/Audition.vue";
 
 const DEFAULT_TITLE = 'AfterLive'
 
@@ -9,6 +10,14 @@ export const routes = [
     name: "Index",
     component: Index,
   },
+  {
+    path: "/audition",
+    name: "Audition",
+    component: Audition,
+    meta: {
+      title: 'Audition | AfterLive'
+    }
+  }
 ];
 
 const router = createRouter({
